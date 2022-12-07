1241d8ec89dc132be9b390fffaa299dd364d9e3f
Flenber Men <89217722666ps@mail.ru>
2022-12-07 04:01:38 +0300

    pr4-11

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('.profile__button-edit');
 const overlay = document.querySelector('.popup');
-const closePopup = document.querySelector('.modal__close');
+const closePopup = document.querySelector('.popup__close');
 const name = document.querySelector('.profile__title');
 const subTitle = document.querySelector('.profile__subtitle');
-const nameinput = document.querySelector('.modal__name');
-const profinput = document.querySelector('.modal__prof');
-const btnSave = document.querySelector('.modal__save');
+const nameinput = document.querySelector('.popup__name');
+const profinput = document.querySelector('.popup__prof');
+const btnSave = document.querySelector('.popup__save');
 const formElement = document.querySelector('.profile__modal-input');
-const popup = document.querySelector('.modal__form');
+const popup = document.querySelector('.popup__form');
 
 function closeOverlay(){
     overlay.classList.remove('popup_open');
